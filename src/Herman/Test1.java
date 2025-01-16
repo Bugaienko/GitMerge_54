3cba9444d352fa99147f89e6577a73254112e8e0
Herman <german1514u@gmail.com>
2025-01-16 20:27:11 +0100

    Test1

---
+++ src/Herman/Test1.java
@@ -0,0 +1,8 @@
+package Herman;
+
+public class Test1 {
+    public static void main(String[] args) {
+        String name = "Herman Ihnatenko";
+        System.out.println(name.toUpperCase());
+    }
+}
