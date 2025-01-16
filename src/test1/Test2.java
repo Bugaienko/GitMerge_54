c1f8cd6ad49fb2ac1538880ba58e4b15e7baba7a
SergeyB <sergiibugaienko@gmail.com>
2025-01-16 18:47:37 +0100

    2 commit

---
+++ src/test1/Test2.java
@@ -0,0 +1,13 @@
+package test1;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 16.01.2025
+ */
+
+public class Test2 {
+
+    public static void testStatic() {
+        System.out.println("testStatic");
+    }
+}
