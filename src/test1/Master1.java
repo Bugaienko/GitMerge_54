1ee2296aa86a512d53e28a97e45caaa1c7d74093
SergeyB <sergiibugaienko@gmail.com>
2025-01-16 18:55:22 +0100

    master

---
+++ src/test1/Master1.java
@@ -0,0 +1,12 @@
+package test1;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 16.01.2025
+ */
+
+public class Master1 {
+    public static void main(String[] args) {
+        System.out.println("Master! ");
+    }
+}
