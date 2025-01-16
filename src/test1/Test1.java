38ed198fc528539acc67b756164260a71699785e
SergeyB <sergiibugaienko@gmail.com>
2025-01-16 18:46:19 +0100

    first commit

---
+++ src/test1/Test1.java
@@ -0,0 +1,13 @@
+package test1;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 16.01.2025
+ */
+
+public class Test1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
