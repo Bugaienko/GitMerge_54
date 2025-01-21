108e7188e1a56c3277b405a52178f467ae2610fd
AnatoliAlt <akademit24@gmail.com>
2025-01-21 16:34:33 +0100

    1 commit

--- src/test1/Sergey.java
+++ src/test1/Sergey.java
@@ -4,6 +4,9 @@ package test1;
  * @author Sergey Bugaenko
  * {@code @date} 16.01.2025
  */
+/*
+1. В пакете src создать пакет с названием, соответствующий вашему имени
+ */
 
 public class Sergey {
     public static void main(String[] args) {
